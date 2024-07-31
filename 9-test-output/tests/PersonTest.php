3c2b980dec8a01297d4cd833f46f183598eed6ab
Arief-Karditya <nolifegansaya@gmail.com>
2024-07-31 08:47:56 +0700

    Test Output, PHP Unit Test

---
+++ 9-test-output/tests/PersonTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace AriefKarditya\TestOutputPhp;
+
+use AriefKarditya\TestOutputPhp\Person;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Assert;
+
+class PersonTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function hello() # Skenario Sukses
+    {
+        $person = new Person("Terry");
+        Assert::assertEquals("Hi Andrew! my name is Terry", $person->sayHello("Andrew"));
+    }
+
+    /**
+     * @test
+     */
+    public function output() # Test Output
+    {
+        $person = new Person("Davis");
+        $this->expectOutputString("Good bye Terry! from Davis." . PHP_EOL);
+        $person->sayGoodBye("Terry");
+    }
+
+    /**
+     * @test
+     */
+    public function exceptionGoodByeNull()
+    {
+        $person = new Person("Davis");
+        $this->expectException(\Exception::class);
+        $person->sayGoodBye(null);
+    }
+
+    /**
+     * @test
+     */
+    public function exceptionGoodByeBlank()
+    {
+        $person = new Person("Davis");
+        $this->expectException(\Exception::class);
+        $person->sayGoodBye("      ");
+    }
+
+    /**
+     * @test
+     */
+    public function exceptionHelloNull()
+    {
+        $person = new Person("Davis");
+        $this->expectException(\Exception::class);
+        $person->sayHello(null);
+    }
+
+    /**
+     * @test
+     */
+    public function exceptionHelloBlank()
+    {
+        $person = new Person("Davis");
+        $this->expectException(\Exception::class);
+        $person->sayHello("        ");
+    }
+}
